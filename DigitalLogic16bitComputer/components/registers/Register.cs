1d5cd60b8c9b1daa9f0249484f0e738821aaff4e
Michael Vaquier <michaelvaquier74@gmail.com>
2023-01-11 13:34:01 -0800

    Added register class and tests

---
+++ DigitalLogic16bitComputer/components/registers/Register.cs
@@ -0,0 +1,25 @@
+﻿using DigitalLogic16bitComputer.components.gates;
+
+namespace DigitalLogic16bitComputer.components.registers
+{
+    public class Register
+    {
+        readonly DFlipFlop inputFlipFlop;
+        readonly DFlipFlop outputFlipFlop;
+
+        Bit Input { get; }
+
+        public Bit Output { get; }
+
+        public Register(Bit input, Bit clk, Bit enable)
+        {
+            this.Input = input;
+            var notClk = new NotGate(clk);
+            var andEnableInput = new AndGate(clk, enable);
+            var andEnableOutput = new AndGate(notClk.Output, enable);
+            this.inputFlipFlop = new DFlipFlop(input, andEnableInput.Output);
+            this.outputFlipFlop = new DFlipFlop(this.inputFlipFlop.Output, andEnableOutput.Output);
+            this.Output = this.outputFlipFlop.Output;
+        }
+    }
+}
---
+++ DigitalLogic16bitComputerTest/components/registers/RegisterTest.cs
@@ -0,0 +1,85 @@
+﻿using DigitalLogic16bitComputer.components;
+using DigitalLogic16bitComputer.components.registers;
+
+
+namespace DigitalLogic16bitComputerTest.registers
+{
+    public class RegisterTest
+    {
+        [Test]
+        public void RegisterInitializationToZero()
+        {
+            var input = new Bit(true);
+            var clk = new Bit();
+            var enable = new Bit(true);
+
+            var register = new Register(input, clk, enable);
+
+            Assert.That(register.Output.Value, Is.EqualTo(false));
+        }
+
+        [Test]
+        public void RegisterChangesInput()
+        {
+            var input = new Bit(true);
+            var clk = new Bit();
+            var enable = new Bit(true);
+
+            var register = new Register(input, clk, enable);
+
+            clk.Value = true;
+
+            Assert.That(register.Output.Value, Is.EqualTo(false));
+
+            clk.Value = false;
+
+            Assert.That(register.Output.Value, Is.EqualTo(true));
+
+            input.Value = false;
+
+            Assert.That(register.Output.Value, Is.EqualTo(true));
+
+            clk.Value = true;
+
+            Assert.That(register.Output.Value, Is.EqualTo(true));
+
+            clk.Value = false;
+
+            Assert.That(register.Output.Value, Is.EqualTo(false));
+        }
+
+        [Test]
+        public void RegisterNoChangeWhenDisabled()
+        {
+            var input = new Bit(true);
+            var clk = new Bit();
+            var enable = new Bit(false);
+
+            var register = new Register(input, clk, enable);
+
+            clk.Value = true;
+
+            Assert.That(register.Output.Value, Is.EqualTo(false));
+
+            clk.Value = false;
+
+            Assert.That(register.Output.Value, Is.EqualTo(false));
+
+            enable.Value = true;
+            clk.Value = true;
+            clk.Value = false;
+            enable.Value = false;
+            input.Value = false;
+
+            Assert.That(register.Output.Value, Is.EqualTo(true));
+
+            clk.Value = true;
+
+            Assert.That(register.Output.Value, Is.EqualTo(true));
+
+            clk.Value = false;
+
+            Assert.That(register.Output.Value, Is.EqualTo(true));
+        }
+    }
+}
