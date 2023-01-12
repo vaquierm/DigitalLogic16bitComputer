674c4d6a07d8fcb866d53fdf3adeb9e7ebb88a9d
Michael Vaquier <michaelvaquier74@gmail.com>
2023-01-11 22:39:55 -0800

    Added full adder component and tests

---
+++ DigitalLogic16bitComputer/components/arithmetic/FullAdder.cs
@@ -0,0 +1,30 @@
+﻿using DigitalLogic16bitComputer.components.gates;
+
+namespace DigitalLogic16bitComputer.components.arithmetic
+{
+    public class FullAdder
+    {
+        Bit InputA { get; }
+        Bit InputB { get; }
+        Bit InputCarry { get; }
+        public Bit Output { get; }
+        public Bit OutputCarry { get; }
+
+        public FullAdder(Bit inputA, Bit inputB, Bit inputCarry)
+        {
+            this.InputA = inputA;
+            this.InputB = inputB;
+
+            this.InputCarry = inputCarry;
+
+            var inputXor = new XorGate(inputA, inputB);
+            var inputAnd = new AndGate(inputA, inputB);
+            var outputXor = new XorGate(inputCarry, inputXor.Output);
+            var carryAnd = new AndGate(inputCarry, inputXor.Output);
+            var outputOr = new OrGate(inputAnd.Output, carryAnd.Output);
+
+            this.Output = outputXor.Output;
+            this.OutputCarry = outputOr.Output;
+        }
+    }
+}
---
+++ DigitalLogic16bitComputerTest/components/arithmetic/FullAdderTest.cs
@@ -0,0 +1,28 @@
+﻿using DigitalLogic16bitComputer.components;
+using DigitalLogic16bitComputer.components.arithmetic;
+
+namespace DigitalLogic16bitComputerTest.components.arithmetic
+{
+    public class FullAdderTest
+    {
+        [TestCase(true, true, true, true, true)]
+        [TestCase(true, true, false, false, true)]
+        [TestCase(true, false, true, false, true)]
+        [TestCase(true, false, false, true, false)]
+        [TestCase(false, true, true, false, true)]
+        [TestCase(false, true, false, true, false)]
+        [TestCase(false, false, true, true, false)]
+        [TestCase(false, false, false, false, false)]
+        public void FullAdderTruthTable(bool inputAVal, bool inputBVal, bool inputCaryVal, bool outputExpected, bool outputCarryExpected)
+        {
+            var inputA = new Bit(inputAVal);
+            var inputB = new Bit(inputBVal);
+            var inputCary = new Bit(inputCaryVal);
+
+            var fullAdder = new FullAdder(inputA, inputB, inputCary);
+            
+            Assert.That(fullAdder.Output.Value, Is.EqualTo(outputExpected));
+            Assert.That(fullAdder.OutputCarry.Value, Is.EqualTo(outputCarryExpected));
+        }
+    }
+}
